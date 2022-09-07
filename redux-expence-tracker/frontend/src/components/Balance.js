b7ae67792078ea9fc72c285b28e327b55d5513a3
pronazmul <developernazmul@gmail.com>
2022-09-07 13:47:37 +0600

    redux-expence-tracker/frontend/ Balance Calculation Feature Done

--- redux-expence-tracker/frontend/src/components/Balance.js
+++ redux-expence-tracker/frontend/src/components/Balance.js
@@ -1,12 +1,20 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const Balance = () => {
+  const { transactions } = useSelector((state) => state.transaction)
+  let balance = transactions.reduce(
+    (acc, curr) =>
+      curr.type === 'income' ? acc + curr.amount : acc - curr.amount,
+    0
+  )
+
   return (
     <div className='top_card'>
       <p>Your Current Balance</p>
       <h3>
-        <span>৳</span>
-        <span>10500</span>
+        <span>৳ </span>
+        <span>{balance}</span>
       </h3>
     </div>
   )
