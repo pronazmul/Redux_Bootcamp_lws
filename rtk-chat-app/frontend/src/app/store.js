1ead77d119b70d93443d17d974abb43a4c916c60
pronazmul <developernazmul@gmail.com>
2022-09-14 20:45:59 +0600

    RTK-chat-app RTK Query Setup done

--- rtk-chat-app/frontend/src/app/store.js
+++ rtk-chat-app/frontend/src/app/store.js
@@ -1,8 +1,17 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore } from '@reduxjs/toolkit'
+import { apiSlice } from '../features/api/apiSlice'
+import authReducer from '../features/auth/authSlice'
+import conversationsReducer from '../features/conversations/conversationsSlice'
+import messagesReducer from '../features/messages/messagesSlice'
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    [apiSlice.reducerPath]: apiSlice.reducer,
+    auth: authReducer,
+    conversations: conversationsReducer,
+    messages: messagesReducer,
   },
-});
+  devTools: process.env.NODE_ENV !== 'production',
+  middleware: (getDefaultMiddlwares) =>
+    getDefaultMiddlwares().concat(apiSlice.middleware),
+})
---
+++ rtk-chat-app/frontend/src/features/auth/authApi.js
@@ -0,0 +1,7 @@
+import { apiSlice } from '../api/apiSlice'
+
+export const authApi = apiSlice.injectEndpoints({
+  endpoints: (builder) => {
+    //Api Endpoints
+  },
+})
