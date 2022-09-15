56b83e53020c18db90a7fcf068c477eeed429c02
pronazmul <developernazmul@gmail.com>
2022-09-15 09:48:23 +0600

    rtk-chat-app Login Registration Featuer Implemented

--- rtk-chat-app/frontend/src/features/auth/authApi.js
+++ rtk-chat-app/frontend/src/features/auth/authApi.js
@@ -1,7 +1,63 @@
 import { apiSlice } from '../api/apiSlice'
+import { userLoggedIn } from './authSlice'
 
 export const authApi = apiSlice.injectEndpoints({
-  endpoints: (builder) => {
-    //Api Endpoints
-  },
+  endpoints: (builder) => ({
+    register: builder.mutation({
+      query: (data) => ({
+        url: '/register',
+        method: 'POST',
+        body: data,
+      }),
+      async onQueryStarted(arg, { queryFulfilled, dispatch }) {
+        try {
+          const result = await queryFulfilled
+          localStorage.setItem(
+            'auth',
+            JSON.stringify({
+              accessToken: result.data.accessToken,
+              user: result.data.user,
+            })
+          )
+          dispatch(
+            userLoggedIn({
+              accessToken: result.data.accessToken,
+              user: result.data.user,
+            })
+          )
+        } catch (error) {
+          //Nothing to do
+        }
+      },
+    }),
+    login: builder.mutation({
+      query: (data) => ({
+        url: '/login',
+        method: 'POST',
+        body: data,
+      }),
+      async onQueryStarted(arg, { queryFulfilled, dispatch }) {
+        try {
+          const result = await queryFulfilled
+          localStorage.setItem(
+            'auth',
+            JSON.stringify({
+              accessToken: result.data.accessToken,
+              user: result.data.user,
+            })
+          )
+          dispatch(
+            userLoggedIn({
+              accessToken: result.data.accessToken,
+              user: result.data.user,
+            })
+          )
+        } catch (error) {
+          //Nothing to do
+        }
+      },
+    }),
+  }),
 })
+
+export const { useLoginMutation, useRegisterMutation } = authApi
