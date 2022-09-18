20cf5d15fe1875819360bed0824d32768eb4fc81
pronazmul <developernazmul@gmail.com>
2022-09-18 10:23:32 +0600

    ***EXPRESS AND SOCKET.IO SETUP ON SERVER***

--- rtk-chat-app/server/server.js
+++ rtk-chat-app/server/server.js
@@ -1,24 +1,31 @@
-const auth = require("json-server-auth");
-const jsonServer = require("json-server");
+const auth = require('json-server-auth')
+const jsonServer = require('json-server')
+const express = require('express')
+const http = require('http')
 
-const server = jsonServer.create();
-const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 9000;
+const app = express()
+const server = http.createServer(app)
+const io = require('socket.io')(server)
+
+global.io = io
+
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+const port = process.env.PORT || 9000
 
 // Bind the router db to the app
-server.db = router.db;
+app.db = router.db
 
-server.use(middlewares);
+app.use(middlewares)
 
 const rules = auth.rewriter({
-    users: 640,
-    conversations: 660,
-    messages: 660,
-});
+  users: 640,
+  conversations: 660,
+  messages: 660,
+})
 
-server.use(rules);
-server.use(auth);
-server.use(router);
+app.use(rules)
+app.use(auth)
+app.use(router)
 
-server.listen(port);
+server.listen(port)
